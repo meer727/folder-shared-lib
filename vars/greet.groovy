854f1b7ca4a7cff7045bced57b7d5191ec27ab31
meerali2 <meeralan6989@gmail.com>
2025-05-14 02:50:43 +0530

    add greet.groovy

---
+++ vars/greet.groovy
@@ -0,0 +1,8 @@
+def call(String name) {
+    def hour = new Date().getHours()
+    def greeting = (hour < 12) ? "Good morning" : 
+                 (hour < 18) ? "Good afternoon" : "Good evening"
+    
+    echo "${greeting}, ${name}! 👋"
+    echo "Library loaded successfully from GitHub!"
+}
